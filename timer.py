b464205f45fac8a88d0ea878e83e4bf57504279a
aramvde <91610784+aramvde@users.noreply.github.com>
2021-11-10 13:59:45 +0100

    Delete timer.py

--- timer.py
+++
@@ -1,9 +0,0 @@
-import time
-
-class Timer:
-    def __init__(self):
-        pass
-    
-    def starttime(self):
-        starttime = time.time()
-        time.sleep(60.0 - ((time.time() - starttime) % 60.0))
